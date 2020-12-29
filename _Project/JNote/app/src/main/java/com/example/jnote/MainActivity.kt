c48d18c225870fbdd0839d94734e1c1aeb0f10ac
Notepad96 <notepad1029@gmail.com>
2020-12-29 23:36:25 +0900

    201229

--- _Project/JNote/app/src/main/java/com/example/jnote/MainActivity.kt
+++ _Project/JNote/app/src/main/java/com/example/jnote/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.jnote
 
+import android.content.res.AssetManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.InputStream
 
 class MainActivity : AppCompatActivity() {
+    val assetManager: AssetManager = resources.assets
+    val inputStream: InputStream = assetManager.open("hanja.txt")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        inputStream.bufferedReader().readLines().forEach {
+            var tok = it.split("\t")
+        }
+
+
     }
 }
