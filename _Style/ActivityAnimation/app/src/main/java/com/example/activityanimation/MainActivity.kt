ec9bd0b8d94432fff1dcf6b44cf2ec9a7d0ed7f1
Notepad96 <notepad1029@gmail.com>
2021-02-02 00:14:43 +0900

    210202

--- _Style/ActivityAnimation/app/src/main/java/com/example/activityanimation/MainActivity.kt
+++ _Style/ActivityAnimation/app/src/main/java/com/example/activityanimation/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.activityanimation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun move(view: View) {
+        var intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
+
+        // 아래로 애니메이션
+        overridePendingTransition(R.anim.down_from, R.anim.down_to)
+
+        // 위로 애니메이션
+        // overridePendingTransition(R.anim.up_from, R.anim.up_to)
     }
 }
