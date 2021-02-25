8ec640afab2f5052d576dd4abb74076600ac58b9
Notepad96 <notepad1029@gmail.com>
2021-02-25 23:16:28 +0900

    210225

--- CustomCalendar/app/src/main/java/com/example/customcalendar/CalendarActivity.kt
+++ CustomCalendar/app/src/main/java/com/example/customcalendar/CalendarActivity.kt
@@ -8,21 +8,26 @@ import kotlinx.android.synthetic.main.activity_calendar.*
 
 class CalendarActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewAdapter: MyAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
+
+    var days
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
 
-        val arr: Array<Int> = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
+
         viewManager = GridLayoutManager(this, 7) // 열 개수(Column Count)
-        viewAdapter = MyAdapter(arr)
+        viewAdapter = MyAdapter()
 
         recyclerView = calendarView.apply {
             setHasFixedSize(true)
             layoutManager = viewManager
             adapter = viewAdapter
         }
+
+        viewAdapter.test()
+
     }
 }
--- CustomCalendar/app/src/main/java/com/example/customcalendar/MyAdapter.kt
+++ CustomCalendar/app/src/main/java/com/example/customcalendar/MyAdapter.kt
@@ -1,14 +1,25 @@
 package com.example.customcalendar
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.list_item.view.*
+import java.util.*
 
 class MyAdapter(private val myData: Array<Int>) :
     RecyclerView.Adapter<MyAdapter.MyViewHolder>()
 {
+    val calendar = Calendar.getInstance()
+    companion object {
+        const val DAYS_OF_WEEK = 7
+        const val ROW = 6
+    }
+
+    var date = arrayListOf<Int>()
+
     class MyViewHolder(val layout: View) : RecyclerView.ViewHolder(layout)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -16,7 +27,7 @@ class MyAdapter(private val myData: Array<Int>) :
             .inflate(R.layout.list_item, parent, false)
 
         val width = parent.width / 7
-        val height = parent.height / 5
+        val height = parent.height / ROW
 
         layout.layoutParams = RecyclerView.LayoutParams(width, height)
 
@@ -25,7 +36,20 @@ class MyAdapter(private val myData: Array<Int>) :
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.layout.day.text = myData[position].toString()
+
+
     }
 
     override fun getItemCount() = myData.size
+
+    fun prevMonth() {
+        if(calendar.get(Calendar.MONTH) == 0) {
+            calendar.set(Calendar.YEAR, calendar.get(Calendar.YEAR) - 1)
+            calendar.set(Calendar.MONTH, calendar.get(Calendar.DECEMBER) - 1)
+        } else {
+            calendar.set(Calendar.MONTH, calendar.get(Calendar.MONTH) - 1)
+        }
+    }
+
+    fun test() = "asb"
 }
