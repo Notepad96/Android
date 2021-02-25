f099ff609903837cfadd182cea06f0ed3e9eb81e
Notepad96 <notepad1029@gmail.com>
2021-02-26 00:48:52 +0900

    210226

--- CustomCalendar/app/src/main/java/com/example/customcalendar/CalendarActivity.kt
+++ CustomCalendar/app/src/main/java/com/example/customcalendar/CalendarActivity.kt
@@ -2,21 +2,21 @@ package com.example.customcalendar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_calendar.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CalendarActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: MyAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
 
-    var days
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
-
 
         viewManager = GridLayoutManager(this, 7) // 열 개수(Column Count)
         viewAdapter = MyAdapter()
@@ -26,8 +26,20 @@ class CalendarActivity : AppCompatActivity() {
             layoutManager = viewManager
             adapter = viewAdapter
         }
+        currentMonth(Calendar.getInstance())
+    }
+    fun currentMonth(calendar: Calendar) {
+        tempDay.text = SimpleDateFormat("yyyy MM").format(calendar.time)
+    }
 
-        viewAdapter.test()
-
+    fun prev(view: View) {
+        viewAdapter.prevMonth()
+        currentMonth(viewAdapter.calendar)
+        viewAdapter.notifyDataSetChanged()
+    }
+    fun next(view: View) {
+        viewAdapter.nextMonth()
+        currentMonth(viewAdapter.calendar)
+        viewAdapter.notifyDataSetChanged()
     }
 }
--- CustomCalendar/app/src/main/java/com/example/customcalendar/MyAdapter.kt
+++ CustomCalendar/app/src/main/java/com/example/customcalendar/MyAdapter.kt
@@ -9,7 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.list_item.view.*
 import java.util.*
 
-class MyAdapter(private val myData: Array<Int>) :
+class MyAdapter() :
     RecyclerView.Adapter<MyAdapter.MyViewHolder>()
 {
     val calendar = Calendar.getInstance()
@@ -17,8 +17,14 @@ class MyAdapter(private val myData: Array<Int>) :
         const val DAYS_OF_WEEK = 7
         const val ROW = 6
     }
+    init {
+        calendar.time = Date()
+    }
 
-    var date = arrayListOf<Int>()
+    var date = IntArray(DAYS_OF_WEEK * ROW) { 0 }
+    var prevDays = 0
+    var currentDays = 0
+    var nextDays = 0
 
     class MyViewHolder(val layout: View) : RecyclerView.ViewHolder(layout)
 
@@ -30,26 +36,57 @@ class MyAdapter(private val myData: Array<Int>) :
         val height = parent.height / ROW
 
         layout.layoutParams = RecyclerView.LayoutParams(width, height)
+        getMonthDays()
 
         return MyViewHolder(layout)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.layout.day.text = myData[position].toString()
-
-
+        holder.layout.day.text = date[position].toString()
     }
 
-    override fun getItemCount() = myData.size
+    override fun getItemCount() = date.size
+
 
     fun prevMonth() {
         if(calendar.get(Calendar.MONTH) == 0) {
             calendar.set(Calendar.YEAR, calendar.get(Calendar.YEAR) - 1)
-            calendar.set(Calendar.MONTH, calendar.get(Calendar.DECEMBER) - 1)
+            calendar.set(Calendar.MONTH, calendar.get(Calendar.DECEMBER))
         } else {
             calendar.set(Calendar.MONTH, calendar.get(Calendar.MONTH) - 1)
         }
+        getMonthDays()
+    }
+    fun nextMonth() {
+        if(calendar.get(Calendar.MONTH) == 11) {
+            calendar.set(Calendar.YEAR, calendar.get(Calendar.YEAR) + 1)
+            calendar.set(Calendar.MONTH, calendar.get(0))
+        } else {
+            calendar.set(Calendar.MONTH, calendar.get(Calendar.MONTH) + 1)
+        }
+        getMonthDays()
     }
 
-    fun test() = "asb"
+    private fun getMonthDays() {
+        var index = 0
+        calendar.set(Calendar.DATE, 1)
+
+        prevDays = calendar.get(Calendar.DAY_OF_WEEK) - 1
+        currentDays = calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
+        nextDays = DAYS_OF_WEEK * ROW - (prevDays + currentDays)
+
+        var prevCalendar = calendar.clone() as Calendar
+        if (prevCalendar.get(Calendar.MONTH) == 0) {
+            prevCalendar.set(Calendar.YEAR, calendar.get(Calendar.YEAR) - 1)
+            prevCalendar.set(Calendar.MONTH, Calendar.DECEMBER)
+        } else {
+            prevCalendar.set(Calendar.MONTH, prevCalendar.get(Calendar.MONTH) - 1)
+        }
+
+        var prevLast = prevCalendar.getActualMaximum(Calendar.DAY_OF_MONTH) - prevDays
+        for( i in 1..prevDays) date[index++] = ++prevLast
+        for( i in 1..currentDays) date[index++] = i
+        for( i in 1..nextDays) date[index++] = i
+    }
+
 }
