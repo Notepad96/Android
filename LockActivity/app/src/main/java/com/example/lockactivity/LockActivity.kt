756759e4d6780154611f4e9ad1bbe44dceaf4155
Notepad96 <notepad1029@gmail.com>
2021-03-09 02:51:13 +0900

    210309

--- LockActivity/app/src/main/java/com/example/lockactivity/LockActivity.kt
+++ LockActivity/app/src/main/java/com/example/lockactivity/LockActivity.kt
@@ -60,21 +60,30 @@ class LockActivity : AppCompatActivity() {
                 var retry = Intent(this, LockActivity::class.java)
                 if(intent.getStringExtra("password") == pwd) {
                     Toast.makeText(this, "${pwd}/${intent.getStringExtra("password")}", Toast.LENGTH_SHORT).show()
-                    sharedPref.edit{
-                        putString("pwd", viewText.joinToString(""))
+                    sharedPref.edit {
+                        putString("pwd", pwd)
                     }
                 } else {
-                    retry.putExtra("password", pwd)
-                    retry.putExtra("second", retry.getIntExtra("second", 0) + 1 )
+                    when(intent.getIntExtra("second", 0) ) {
+                        0 -> {
+                            retry.putExtra("password", pwd)
+                            retry.putExtra("second", 1)
+                        }
+                        else -> {
+                            retry.putExtra("password", intent.getStringExtra("password"))
+                            retry.putExtra("second", 2)
+                        }
+                    }
                 }
                 startActivity(retry)
                 finish()
             }
             else if(pwd == sharedPref.getString("pwd", null)) {
                 startActivity(Intent(this, MainActivity::class.java))
+                Toast.makeText(this, "접속 성공!!", Toast.LENGTH_SHORT).show()
                 finish()
             } else {
-                Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+//                Toast.makeText(this, "${pwd}/${sharedPref.getString("pwd", null)}", Toast.LENGTH_SHORT).show()
 //                Toast.makeText(this, "$pwd", Toast.LENGTH_SHORT).show()
                 count = 0
                 viewText = arrayListOf<String>("○", "○", "○", "○", "○", "○")
