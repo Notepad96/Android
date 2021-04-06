66f5a9ce7e68b63d80462dadcb691aa2c2391625
Notepad96 <notepad1029@gmail.com>
2021-04-06 21:59:07 +0900

    210406

--- ImageLoad/app/src/main/java/com/example/imageload/MainActivity.kt
+++ ImageLoad/app/src/main/java/com/example/imageload/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.imageload
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import coil.load
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        imgView.load("https://cdn.pixabay.com/photo/2013/08/20/15/47/poppies-174276__180.jpg")
+
+//        imgView.load(R.drawable.image_back)
+
+        imgView.load(File("image0.jpg"))
     }
 }
