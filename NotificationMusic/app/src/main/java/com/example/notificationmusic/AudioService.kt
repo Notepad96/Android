e38c81f96033e1dd962de73699184ad0915a6f34
Notepad96 <notepad1029@gmail.com>
2021-04-14 20:32:33 +0900

    210414

--- NotificationMusic/app/src/main/java/com/example/notificationmusic/AudioService.kt
+++ NotificationMusic/app/src/main/java/com/example/notificationmusic/AudioService.kt
@@ -2,10 +2,24 @@ package com.example.notificationmusic
 
 import android.app.Service
 import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Binder
 import android.os.IBinder
 
-class AudioService : Service {
-    val binder = LocalBinder()
+class AudioService : Service() {
+    var binder = AudioServiceBinder()
+    lateinit var mediaPlayer: MediaPlayer
+    var isPrepared: Boolean = true
+
+    class AudioServiceBinder : Binder() {
+        fun getService(): AudioService {
+            return AudioService()
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
 
     override fun onBind(intent: Intent?): IBinder? {
         return binder
