bc6f461a46385654aa47c8e6cade1052104c296c
Notepad96 <notepad1029@gmail.com>
2021-04-25 21:01:51 +0900

    210425

---
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/MyAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.musicplayer
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    class MyViewHolder(val layout: View): RecyclerView.ViewHolder(layout)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
