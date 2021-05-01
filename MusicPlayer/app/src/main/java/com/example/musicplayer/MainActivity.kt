688757b3e424709297a6ee7d1b55a12b753d3a8f
Notepad96 <notepad1029@gmail.com>
2021-05-01 16:23:10 +0900

    210501

--- MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -14,6 +14,7 @@ import android.widget.SeekBar
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import kotlinx.android.synthetic.main.activity_main.*
@@ -44,6 +45,15 @@ class MainActivity : AppCompatActivity() {
 
         checkPermissions()
         getAudioList()
+
+        viewManager = LinearLayoutManager(applicationContext)
+        viewAdapter = MyAdapter(datas)
+
+        recyclerView = listMusic.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
     }
 
 
