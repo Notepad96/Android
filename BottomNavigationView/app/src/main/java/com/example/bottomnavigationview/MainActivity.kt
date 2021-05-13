46b7220c100bcd787615f26e96d34f7815930737
Notepad96 <notepad1029@gmail.com>
2021-05-13 23:33:22 +0900

    210513

--- BottomNavigationView/app/src/main/java/com/example/bottomnavigationview/MainActivity.kt
+++ BottomNavigationView/app/src/main/java/com/example/bottomnavigationview/MainActivity.kt
@@ -4,26 +4,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavView.setOnNavigationItemSelectedListener(this)
+
+        supportFragmentManager.beginTransaction().replace(R.id.mainLayout, Fragment1()).commit()
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
             R.id.home -> {
-
+                supportFragmentManager.beginTransaction().replace(R.id.mainLayout, Fragment1()).commit()
             }
             R.id.service -> {
-
+                supportFragmentManager.beginTransaction().replace(R.id.mainLayout, Fragment2()).commit()
             }
             R.id.setting -> {
-
+                supportFragmentManager.beginTransaction().replace(R.id.mainLayout, Fragment3()).commit()
             }
         }
-        return false
+        return true
     }
 
 }
