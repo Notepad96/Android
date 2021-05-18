ed0b7ebf7943e53b9d9da9fd3f8bb4b1daff29b1
Notepad96 <notepad1029@gmail.com>
2021-05-19 01:49:12 +0900

    210519

--- CordinatorLayout/app/src/main/java/com/example/cordinatorlayout/MainActivity.kt
+++ CordinatorLayout/app/src/main/java/com/example/cordinatorlayout/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.cordinatorlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar.title = "TEST TEXT"
+        setSupportActionBar(toolbar)
+        if(supportActionBar != null) supportActionBar?.setDisplayHomeAsUpEnabled(false)
     }
 }
