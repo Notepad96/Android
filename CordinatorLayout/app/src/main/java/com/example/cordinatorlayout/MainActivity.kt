7200d778e0f1b2bd57b7859817a599da580bf8d7
Notepad96 <notepad1029@gmail.com>
2021-05-22 15:06:11 +0900

    210522

--- CordinatorLayout/app/src/main/java/com/example/cordinatorlayout/MainActivity.kt
+++ CordinatorLayout/app/src/main/java/com/example/cordinatorlayout/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        toolbar.title = "TEST TEXT"
+
         setSupportActionBar(toolbar)
         if(supportActionBar != null) supportActionBar?.setDisplayHomeAsUpEnabled(false)
     }
