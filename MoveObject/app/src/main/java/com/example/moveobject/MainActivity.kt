5f781f1a406934b6460f310c0c3500b7f4f34c1a
Notepad96 <notepad1029@gmail.com>
2021-08-14 03:19:11 +0900

    210814

--- MoveObject/app/src/main/java/com/example/moveobject/MainActivity.kt
+++ MoveObject/app/src/main/java/com/example/moveobject/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.moveobject
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.widget.Button
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var myLayout: MyLayout
     var fixed = true
 
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         addBtn.setOnClickListener {
             myLayout = MyLayout(this)
             mainLayout.addView(myLayout.getLayout())
-            mainLayout.gravity = Gravity.CENTER
+            text01.text = resources.getXml(R.layout.activity_main).toString()
         }
 
         fixBtn.setOnClickListener {
