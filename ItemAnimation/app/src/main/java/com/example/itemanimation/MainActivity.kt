633a56679b68e52ee22a960ee5794307a10f8b85
Notepad96 <notepad1029@gmail.com>
2021-09-16 19:41:54 +0900

    item animation

---
+++ ItemAnimation/app/src/main/java/com/example/itemanimation/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.itemanimation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var recyclerView: RecyclerView
+    lateinit var viewAdapter: RecyclerView.Adapter<*>
+    lateinit var viewManager: RecyclerView.LayoutManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewManager = GridLayoutManager(this, 3)
+        viewAdapter = MyAdapter()
+
+        recyclerView = testList.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+    }
+}
---
+++ ItemAnimation/app/src/main/java/com/example/itemanimation/MyAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.itemanimation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_item.view.*
+import kotlinx.android.synthetic.main.list_last_item.view.*
+
+class MyAdapter: RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+    val LAST_ITEM = 1
+    val ITEM = 0
+
+    var myData = listOf("qweqrtqw", "ZXcasd", "etwadfsd")
+
+    class MyViewHolder(val layout: View): RecyclerView.ViewHolder(layout)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return if(viewType == LAST_ITEM) {
+            MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_last_item, parent, false))
+        } else {
+            MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false))
+        }
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        if(position != myData.size) holder.layout.textItem.text = myData[position]
+        else {
+            holder.layout.testImage.setOnClickListener {
+                Toast.makeText(holder.layout.context, "Click Test", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return myData.size + 1
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if(position == myData.size) {
+            LAST_ITEM
+        } else {
+            ITEM
+        }
+    }
+}
